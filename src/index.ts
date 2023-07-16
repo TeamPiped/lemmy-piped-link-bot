5ae85f2cc4de44e1e492ca6ae3dd0f2b84a96682
Bishop Clark <bishopolis@gmail.com>
2023-07-15 21:47:31 -0700

    Update index.ts
    - Remove Comma splice

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const findYoutubeLinks = (text: string): string[] => {
 const generateLinkMessage = (links: string[]): string => {
     return `Here is an alternative Piped link(s): ${links.join(
         "\n\n",
-    )}\n\nPiped is a privacy-respecting open-source alternative frontend to YouTube.\n\nI'm open-source, check me out at [GitHub](https://github.com/TeamPiped/lemmy-piped-link-bot).`;
+    )}\n\nPiped is a privacy-respecting open-source alternative frontend to YouTube.\n\nI'm open-source; check me out at [GitHub](https://github.com/TeamPiped/lemmy-piped-link-bot).`;
 };
 
 const username = process.env.USERNAME || "PipedLinkBot";
